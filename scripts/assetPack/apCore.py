115fab8095888b0a4d42f61720e77c830cb3a209
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2024-04-07 22:05:29 +0100

    adding examples for asset pack and adding load asset pack func

--- scripts/assetPack/apCore.py
+++ scripts/assetPack/apCore.py
@@ -1,6 +1,6 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
-
+import json
 def createCustomPointNode(referenceFile="",transform=[0,0,0],rotation=[0,0,0],scale=[1,1,1]):
     cpn = cmds.createNode('customPointNode')
     print(cpn)
@@ -12,17 +12,59 @@ def createCustomPointNode(referenceFile="",transform=[0,0,0],rotation=[0,0,0],sc
         name = referenceFile.split('/')[-1].split('_V')[0]
         cmds.rename(cpn,name)
     return cpn
+
+
 def createCustomPointNodeFromDict(dict={}):
     rFile="D:/test/assetPack_dept_element_V001.abc"
-    if 'referenceFile' in dict.keys():
+    keys = dict.keys()
+    if 'referenceFile' in keys:
         if dict['referenceFile'] is not '':
             rFile=dict['referenceFile']
         else:
             print('blank reference file')
     else:
         print("No reference file found")
-    cpn = createCustomPointNode(referenceFile=rFile)
+    transform = [0,0,0]
+    rotation = [0,0,0]
+    scale = [1,1,1]
+    if "rotation" in keys:
+        rotation = dict['rotation']
+    if "transform" in keys:
+        transform = dict['transform']
+    if "scale" in keys:
+        scale =dict['scale']
+    cpn = createCustomPointNode(referenceFile=rFile,transform=transform,rotation=rotation,scale=scale)
     return cpn
+
+
+def jsonFileToDict(filepath):
+    """
+    Converts a JSON file to a Python dictionary.
+    Args:
+        filepath (str): Path to the JSON file.
+    Returns:
+        dict: The contents of the JSON file as a dictionary.
+    """
+    try:
+        with open(filepath, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except:
+        print("Error: File  not found.")
+        return None
+    
+
 def unpackSelectedAssetPacks():
+    """placeholder pseudo code"""
     pass
-createCustomPointNodeFromDict()
+
+def loadAssetPack(filepath):
+    myDict = jsonFileToDict(filepath)
+    print(myDict)
+    createCustomPointNodeFromDict(dict=myDict)
+import glob
+fp = 'D:/Tools/maya/maya_lgmfx/examples/json/assetPack_example.json'    
+a=glob.glob(fp)
+print(a)
+loadAssetPack(filepath=fp)
+#need to fix rename bug
