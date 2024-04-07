811ddf4d70c68decdc8cff15019c2f988cdbaabc
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2024-04-07 19:25:09 +0100

    basic prototype of asset pack core

--- scripts/assetPack/apCore.py
+++ scripts/assetPack/apCore.py
@@ -1,3 +1,28 @@
 import maya.cmds as cmds
-def createCustomPointNode():
-    cmds.createNode('customPointNode')
+import maya.OpenMaya as om
+
+def createCustomPointNode(referenceFile="",transform=[0,0,0],rotation=[0,0,0],scale=[1,1,1]):
+    cpn = cmds.createNode('customPointNode')
+    print(cpn)
+    cmds.setAttr(cpn+'.referenceFile',referenceFile,type="string")
+    cmds.setAttr(cpn+'.transform',transform[0],transform[1],transform[2])
+    cmds.setAttr(cpn+'.scale',scale[0],scale[1],scale[2])
+    cmds.setAttr(cpn+'.rotation',rotation[0],rotation[1],rotation[2])
+    if referenceFile is not "":
+        name = referenceFile.split('/')[-1].split('_V')[0]
+        cmds.rename(cpn,name)
+    return cpn
+def createCustomPointNodeFromDict(dict={}):
+    rFile="D:/test/assetPack_dept_element_V001.abc"
+    if 'referenceFile' in dict.keys():
+        if dict['referenceFile'] is not '':
+            rFile=dict['referenceFile']
+        else:
+            print('blank reference file')
+    else:
+        print("No reference file found")
+    cpn = createCustomPointNode(referenceFile=rFile)
+    return cpn
+def unpackSelectedAssetPacks():
+    pass
+createCustomPointNodeFromDict()
